a5616e477c020ba22e0dedb92dd717c1993bcacf
RadiyX <sigor00@mail.ru>
2025-08-09 18:08:45 +0700

    WebSockets support added

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
+PLUGIN ?= none
+
 up:
-	cd _docker && docker-compose --env-file ../.env up -d
+	cd _docker && docker compose --env-file ../.env up -d
 
 down:
-	cd _docker && docker-compose --env-file ../.env down
+	cd _docker && docker compose --env-file ../.env down
 
 restart:
 	make down
 	make up
+
+shell:
+	cd _docker && docker compose --env-file ../.env exec vst-host bash
+
+build:
+	cd _docker && docker compose --env-file ../.env -f docker-compose.build.yml up -d --build
+	cd _docker && docker compose exec vst-host-build cd /plugins/${PLUGIN}/build && cmake .. && make
+	cd _docker && docker compose --env-file ../.env -f docker-compose.build.yml down
--- app/Program.cs
+++ app/Program.cs
@@ -1,32 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+app.MapGet("/healthcheck", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return "Everything work's fine";
 });
 
-app.Run();
+app.UseRouting();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+var webSocketOptions = new WebSocketOptions
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+//webSocketOptions.AllowedOrigins.Add("https://client.com");
+
+app.UseWebSockets(webSocketOptions);
+
+app.MapControllers();
+
+app.Run();
